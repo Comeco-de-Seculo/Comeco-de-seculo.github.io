d9b939f4f1f1987999cb3341cf28a9dfdff4d1ee
gidagel <gidagel@gmail.com>
2021-08-24 18:49:30 +0200

    Deploy

---
+++ _next/static/EfXCLn7C9hsCVXTqlOpcy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/132-6c95cf93864800775c38.js","static/chunks/pages/index-d69be3b1f230b8316e0a.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
